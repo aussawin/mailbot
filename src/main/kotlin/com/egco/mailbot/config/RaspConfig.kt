f5c189def4f6798a3dee7c12e187498e1af3250f
aussawin <Aussawin@Aussawins-MacBook-Air.local>
2017-09-26 20:04:03 +0700

    Add raspberry baseURL and sent something to rasp

---
+++ src/main/kotlin/com/egco/mailbot/config/RaspConfig.kt
@@ -0,0 +1,8 @@
+package com.egco.mailbot.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RaspConfig {
+    var baseUrl: String = "server/call"
+}
--- src/main/kotlin/com/egco/mailbot/controller/BotController.kt
+++ src/main/kotlin/com/egco/mailbot/controller/BotController.kt
@@ -1,27 +1,36 @@
 package com.egco.mailbot.controller
 
+import com.egco.mailbot.config.RaspConfig
 import com.egco.mailbot.dao.CallingReq
 import com.egco.mailbot.dao.LogForm
 import com.egco.mailbot.domain.Log
 import com.egco.mailbot.repository.LogRepository
 import com.egco.mailbot.repository.UserRepository
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.RestTemplate
 import java.util.*
 
 @RestController
 @RequestMapping(value = "/api/controller")
 @CrossOrigin("*")
 class BotController(val userRepository: UserRepository,
-                    val logRepository: LogRepository) {
+                    val logRepository: LogRepository,
+                    val raspConfig: RaspConfig) {
 
     @RequestMapping(value = "/call", method = arrayOf(RequestMethod.POST))
     fun calling(@RequestBody req: CallingReq): String{
         val loc: Int = 1
         val log: Log = Log(req.sender, loc, req.target, loc, req.subject, req.note, "Calling", Date())
         logRepository.save(log)
+
+        var tmpInt: TemplateInt = TemplateInt(1)
+
+        var restTemplate: RestTemplate = RestTemplate()
+        var retsult: TemplateInt = restTemplate.postForObject(raspConfig.baseUrl, tmpInt, TemplateInt::class.java)
+
         return "Success"
     }
-
+    
     @RequestMapping(value = "/history", method = arrayOf(RequestMethod.GET))
     fun showHistory(): ArrayList<LogForm>{
         val log: ArrayList<Log> = logRepository.findBySenderContainingOrderByCreatedAt("user")!!
@@ -30,3 +39,4 @@ class BotController(val userRepository: UserRepository,
         return logList
     }
 }
+data class TemplateInt(var i: Int)
